6ab33fcd665cdae86a1acb07bbe339eaab736d3a
kenankaplan-codeliman <kenan.kaplan@codeliman.com>
2026-03-09 13:05:47 +0300

    AppLoginRepository

--- CRM.Api/Configuration/DependencyInjection.cs
+++ CRM.Api/Configuration/DependencyInjection.cs
@@ -43,6 +43,7 @@ public static class DependencyInjection
         services.AddScoped<IContactRepository, ContactRepository>();
         services.AddScoped<IOpportunityRepository, OpportunityRepository>();
         services.AddScoped<IProductRepository, ProductRepository>();
+        services.AddScoped<IAppLoginRepository, AppLoginRepository>();
 
 
         //Command Handler
